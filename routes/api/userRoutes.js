356a0ae7f0c1b4881ef428f05b61bfc8914f9569
lukehevey <lukehevey8@gmail.com>
2023-10-19 11:31:38 -0500

    Thought and user routes created

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/userss/:userId/friends
+// router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
