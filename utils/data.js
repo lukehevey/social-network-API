5221689c21e6b489b20907fd69d3b448d55136c5
lukehevey <lukehevey8@gmail.com>
2023-10-19 11:32:10 -0500

    Placeholder data created

---
+++ utils/data.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+        "username": "lernantino",
+        "email": "lernantino@gmail.com"
+      },
+      {
+        "username": "sal",
+        "email": "sal@gmail.com"
+      }, {
+        "username": "antonio",
+        "email": "antonio@gmail.com"
+      },
+];
+
+const thoughts = [
+    {
+        "thoughtText": "Here's a cool thought...",
+        "username": "lernantino",
+      },
+      {
+        "thoughtText": "Here's a 2nd cool thought...",
+        "username": "sal",
+      },
+      {
+        "thoughtText": "Here's a 3rd ool thought...",
+        "username": "antonio",
+      },
+]
+
+module.exports = { users, thoughts }
